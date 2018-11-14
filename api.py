1ce530b9581f250fb4c96f196d8adbd79aafe1e7
Haeryn Kim <hk181@duke.edu>
2018-11-13 22:52:25 -0500

    Add main function.

--- api.py
+++ api.py
@@ -2,7 +2,7 @@
 # This code contains all the functions required to interact (e.g. get or post requests) with a server.
 # Modified Nov 13, 2018
 # Written by Haeryn Kim
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,7 @@ app = Flask(__name__)
 def parse(dict, str):
     """ Finds key associated with an input string in the input dictionary, and returns the key's value.
     Tests if the key is a string, and gives an error statement.
+    Tests if key is in the dictionary.
     Tests if dict is a jsonified dictionary.
 
     :param dict: dictionary
@@ -19,7 +20,7 @@ def parse(dict, str):
     return dict[str]
 
 def add_to_dictionary(newdict, key, val):
-    """ Adds the input key and value pair into a dictionary called dict.
+    """ Adds the given key and value pair into a dictionary called dict.
 
     :param newdict: dictionary
     :param key: string
@@ -38,9 +39,12 @@ def post_new_patient():
 
     :return: jsonified dictionary
     """
-    patient = requests.get_json()  # parse through the request from client passed as json dictionary
+    patient = request.get_json()  # parse through the request from client passed as json dictionary
     #Do I need to declare s as a new dictionary item before I call it?
     add_to_dictionary(s, "patient_id", parse(patient, "patient_id"))  # write code here that is
     add_to_dictionary(s, "attending_email", parse(patient, "attending_email"))
     add_to_dictionary(s, "user_age", parse(patient, "user_age"))
-    return s, 200 # output status code reflecting that an entity has been posted as result of the function
+    return jsonify(s), 200 # output status code reflecting that an entity has been posted as result of the function
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1") # IP needs swapped out if running on VM.

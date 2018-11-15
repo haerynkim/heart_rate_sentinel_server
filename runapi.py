2450c128ce5d78793078de194954adb81ff1987b
Haeryn Kim <hk181@duke.edu>
2018-11-14 23:36:01 -0500

    Replace code that returns the request with a print statement

--- runapi.py
+++ runapi.py
@@ -1,11 +1,10 @@
 import requests
-from flask import jsonify
 
 def send_new_patient():
-    r = requests.post("http://127.0.0.1:5000/api/new_patient", {"patient_id":"1",
+    r = requests.post("http://127.0.0.1:5000/api/new_patient", json = {"patient_id":"1",
                                                             "attending_email":"suyash.kumar@duke.edu",
                                                             "user_age":52})
-    return jsonify(r)
+    print(r.json())
 
 if __name__ == "__main__":
     send_new_patient()

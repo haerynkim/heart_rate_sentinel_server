492e661daf19ebdad6ffafc8685dc942b29b17a5
Haeryn Kim <hk181@duke.edu>
2018-11-16 14:53:36 -0500

    Change client function for get_status

--- run_post_new_patient.py
+++ run_post_new_patient.py
@@ -3,7 +3,7 @@ import requests
 def send_new_patient():
     r = requests.post("http://127.0.0.1:5000/api/new_patient", json = {"patient_id":"1",
                                                             "attending_email":"suyash@duke.edu",
-                                                            "user_age":50})
+                                                            "user_age":4})
     #print(r.json())
 
 if __name__ == "__main__":

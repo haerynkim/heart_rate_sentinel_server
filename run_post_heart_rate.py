e6f0851bce61e7a160e85bae26f6b41170f7618b
Haeryn Kim <hk181@duke.edu>
2018-11-16 14:46:15 -0500

    Add client function to run get request for status/<patient_id>. Code correctly determines if patient is tachycardic and prints a return statement.

--- run_post_heart_rate.py
+++ run_post_heart_rate.py
@@ -2,9 +2,9 @@ import requests
 
 
 def send_heart_rate():
-    r = requests.post("http://127.0.0.1:5000/api/heart_rate", json={"patient_id": "0",
-                                                                    "heart_rate": 88})
-    print(r.text)
+    r = requests.post("http://127.0.0.1:5000/api/heart_rate", json={"patient_id": "2",
+                                                                    "heart_rate": 188})
+    #print(r.text)
     # right now, major error where you must run post_new_patient first.
 
 if __name__ == "__main__":

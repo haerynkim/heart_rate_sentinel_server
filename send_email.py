a2bb9e4b347197af44bbe0e7a33f75d7c304d8f6
Haeryn Kim <hk181@duke.edu>
2018-11-16 19:10:04 -0500

    Add python sketch that successfully sends email.

---
+++ send_email.py
@@ -0,0 +1,17 @@
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+
+if __name__ == "__main__":
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("test@example.com")
+    to_email = Email("haeryn.kim@outlook.com")
+    subject = "Sending with SendGrid is Fun"
+    content = Content("text/plain", "and easy to do anywhere, even with Python")
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
